5c69ebf36eeb72b36e68d1074b7e90b4aca1dfc7
blackjok3r <blackjok3r@gmail.com>
2018-09-05 21:43:03 +0800

    I think this is it!

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Fetch pubkey
 pubkey=$(./printkey.py pub)
-passphrase=$(./printkey.py wif)
-cp m_notary_blank m_notary_staked
 
 # Start KMD
 echo "[KMD] : Starting KMD"
@@ -23,19 +21,8 @@ echo "[KMD] : $(./validateaddress.sh KMD)"
   # Move our auto generated coins file to the iguana coins dir
   chmod +x "$chain"_7776
   mv "$chain"_7776 iguana/coins
-  cat iguana/coins/"$chain"_7776 >> m_notary_staked
-  echo $'\n' >> m_notary_staked
   echo "[$chain] : $(./validateaddress.sh $chain)"
 done
-
-echo "passphrase=$passphrase" > /home/$USER/SuperNET/iguana/passphrase.txt
-echo "sleep 3" >> m_notary_staked
-echo "cd /home/$USER/StakedNotary" >> m_notary_staked
-echo "./dpowassets.py" >> m_notary_staked
-mv m_notary_staked /home/$USER/SuperNET/iguana/
-cp wp_7776 /home/$USER/SuperNET/iguana/
-
 echo "Building Iguana"
-#./build_iguana
-echo "pubkey=$pubkey" > /home/$USER/SuperNET/iguana/pubkey.txt
+./build_iguana
 echo "Finished: Please check ALL your chains are synced before running start_iguana.sh"
