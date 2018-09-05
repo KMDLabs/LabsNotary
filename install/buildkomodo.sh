73a8efa520d2aa5c3d99463c6058a170a59fea5c
Alrighttt <MarkZO@gmail.com>
2018-09-05 22:57:24 +0000

    change repo

--- install/buildkomodo.sh
+++ install/buildkomodo.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get -y install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl4-openssl-dev bsdmainutils automake curl
 #Install Komodo
 cd ~
-git clone https://github.com/libscott/komodo.git -b staked
+git clone https://github.com/stakedchain/komodo.git -b nntest
 cd komodo
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh -j$(nproc)
