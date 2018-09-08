da864611a3676d032799c9f4a8f3037cb7a1286b
Alrighttt <MarkZO@gmail.com>
2018-09-08 14:04:09 +0000

    update keys

--- start_iguana.sh
+++ start_iguana.sh
@@ -10,6 +10,11 @@ sleep 3
 #ADD NOTARY AREA
 curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"45.63.3.143\"}"
 curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"45.63.14.153\"}"
+curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"149.28.237.202\"}"
+curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"195.201.150.200\"}"
+curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"63.209.33.34\"}"
+curl --url "http://127.0.0.1:7776" --data "{\"agent\":\"iguana\",\"method\":\"addnotary\",\"ipaddr\":\"149.28.237.202\"}"
+
 #ADD NOTARY AREA
 #
 
