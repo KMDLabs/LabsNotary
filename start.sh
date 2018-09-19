d52a1a0aedea9427c7a11f1ae652b3e12c87d72f
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-19 18:39:20 +0800

    check chain sync to start iguana
    checks komodo and all staked assetchains sync status, and either runs iguana or not depending on outcome.

--- start.sh
+++ start.sh
@@ -25,4 +25,43 @@ echo "[KMD] : $(./validateaddress.sh KMD)"
 done
 echo "Building Iguana"
 ./build_iguana
-echo "Finished: Please check ALL your chains are synced before running start_iguana.sh"
+
+echo "Finished: Checking chains are in sync..."
+
+let numchains=1
+let count=0;
+kmdinfo=$(echo $(komodo-cli getinfo))
+kmd_blocks=$(echo ${kmdinfo} | jq -r '.blocks')
+kmd_longestchain=$(echo ${kmdinfo} | jq -r '.longestchain')
+if [[ $kmd_blocks == $kmd_longestchain ]]; then
+	let count=count+1;
+	echo "[Komodo chain syncronised on block ${kmd_blocks}]"
+else
+	echo "[Komodo chain not syncronised. On block ${kmd_blocks} of ${kmd_longestchain}]"
+fi
+
+
+ac_json=$(curl https://raw.githubusercontent.com/blackjok3rtt/StakedNotary/master/assetchains.json 2>/dev/null)
+for row in $(echo "${ac_json}" | jq  -r '.[].ac_name'); do
+	let numchains=numchains+1;
+	chain=$(echo $row)
+	info=$(echo $(komodo-cli -ac_name=${chain} getinfo))
+	blocks=$(echo ${info} | jq -r '.blocks')
+	longestchain=$(echo ${info} | jq -r '.longestchain')
+	if [[ $blocks == $longestchain ]]; then
+		let count=count+1;
+		echo "[${chain} chain syncronised on block ${blocks}]"
+	else
+		echo "[${chain} chain not syncronised. On block ${kmd_blocks} of ${kmd_longestchain}]"
+	fi
+done
+
+if [[ $count == $numchains ]]; then
+	echo "[ ALL SYSTEMS GO! ${count} / ${numchains} chains sync'd ]"
+	echo "Starting Iguana..."
+	./start_iguana.sh
+else
+	echo "[ NOT ALL CHAINS IN SYNC (${count} / ${numchains}), ABORTING ]"
+	echo "Please check ALL your chains are synced before running start_iguana.sh"
+fi
+
