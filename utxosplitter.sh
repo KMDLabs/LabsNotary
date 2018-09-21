cbe26044f7ed3084bcf3e5c0962b4f752cd576af
blackjok3rtt <blackjok3r@gmail.com>
2018-09-21 07:03:33 +0000

    Fix utxo split possible runaway split if komodo-cli doesnt exist

--- utxosplitter.sh
+++ utxosplitter.sh
@@ -41,20 +41,24 @@ fi
 
     satoshis=10000
     amount=$(calc $satoshis/100000000)
+    listunspent=$(${cli} listunspent)
+    if [[ "${listunspent}" = "" ]]; then
+      echo "[$coin] Listuspent call failed aborting!"
+    else
+      utxo_count=$(echo ${listunspent} | jq -r '.[].amount' | grep ${amount} | wc -l)
+      echo "[${coin}] Current UTXO count is ${utxo_count}"
 
-    utxo_count=$(${cli} listunspent | jq -r '.[].amount' | grep ${amount} | wc -l)
-    echo "[${coin}] Current UTXO count is ${utxo_count}"
+      utxo_required=$(calc ${target_utxo_count}-${utxo_count})
 
-    utxo_required=$(calc ${target_utxo_count}-${utxo_count})
-
-    if [[ ${utxo_required} -gt ${split_threshold} ]]; then
-      echo "[${coin}] Splitting ${utxo_required} extra UTXOs"
-      json=$(./splitfunds.sh ${coin} ${utxo_required})
-      txid=$(echo ${json} | jq -r '.txid')
-      if [[ ${txid} != "null" ]]; then
-        echo "[${coin}] Split TXID: ${txid}"
-      else
-        echo "[${coin}] Error: $(echo ${json} | jq -r '.error')"
+      if [[ ${utxo_required} -gt ${split_threshold} ]]; then
+        echo "[${coin}] Splitting ${utxo_required} extra UTXOs"
+        json=$(./splitfunds.sh ${coin} ${utxo_required})
+        txid=$(echo ${json} | jq -r '.txid')
+        if [[ ${txid} != "null" ]]; then
+          echo "[${coin}] Split TXID: ${txid}"
+        else
+          echo "[${coin}] Error: $(echo ${json} | jq -r '.error')"
+        fi
       fi
     fi
   fi
