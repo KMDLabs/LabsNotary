c540d26a427889278c960c04b804643ce47534f8
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-25 19:33:43 +0800

    added looping to sync check
    throws non-fatal errors if `bc` not installed (sudo apt install bc)

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Fetch pubkey
+cd /home/$USER/StakedNotary
 pubkey=$(./printkey.py pub)
 
 # Start KMD
@@ -28,40 +29,40 @@ echo "Building Iguana"
 
 echo "Finished: Checking chains are in sync..."
 
-let numchains=1
-let count=0;
 kmdinfo=$(echo $(komodo-cli getinfo))
 kmd_blocks=$(echo ${kmdinfo} | jq -r '.blocks')
 kmd_longestchain=$(echo ${kmdinfo} | jq -r '.longestchain')
-if [[ $kmd_blocks == $kmd_longestchain ]]; then
-	let count=count+1;
-	echo "[Komodo chain syncronised on block ${kmd_blocks}]"
-else
-	echo "[Komodo chain not syncronised. On block ${kmd_blocks} of ${kmd_longestchain}]"
+
+# yet to test version checking - "longest chain can get lost quite easily on early chains, might need some extra check 
+# @webworker01 has some code that checks explorers heights, which might be a good way to go, but we have to have explorers up first.
+if [[ $kmd_longestchain == 0 ]]; then
+        echo -e "\e[91m ** [Incompatible Komodo version. Join #staked on discord at https://discord.gg/tKRzWe to get latest version. ** \e[39m"
+        exit 0;
 fi
 
+while [[ $kmd_blocks < $kmd_longestchain ]]; do
+	kmd_progress=$(echo $kmd_blocks/$kmd_longestchain|bc)
+	echo "[Komodo chain not syncronised. On block ${kmd_blocks} of ${kmd_longestchain}] $(echo $progress*100|bc)%"
+        echo "will check again in 30 seconds"
+	sleep 30
+done
+echo "[Komodo chain syncronised on block ${kmd_blocks}]"
 
 ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
 for row in $(echo "${ac_json}" | jq  -r '.[].ac_name'); do
-	let numchains=numchains+1;
 	chain=$(echo $row)
 	info=$(echo $(komodo-cli -ac_name=${chain} getinfo))
 	blocks=$(echo ${info} | jq -r '.blocks')
 	longestchain=$(echo ${info} | jq -r '.longestchain')
-	if [[ $blocks == $longestchain ]]; then
-		let count=count+1;
-		echo "[${chain} chain syncronised on block ${blocks}]"
-	else
-		echo "[${chain} chain not syncronised. On block ${kmd_blocks} of ${kmd_longestchain}]"
-	fi
+
+	while [[ $blocks < $longestchain ]]; do
+		progress=$(echo blocks/longestchain|bc)
+	        echo "[${chain} chain not syncronised. On block ${blocks} of ${longestchain}] $(echo $progress*100 | bc)%"
+		echo "will check again in 30 seconds"
+        	sleep 30
+	done
+	echo "[${chain} chain syncronised on block ${blocks}]"
 done
 
-if [[ $count == $numchains ]]; then
-	echo "[ ALL SYSTEMS GO! ${count} / ${numchains} chains sync'd ]"
-	echo "Starting Iguana..."
-	./start_iguana.sh
-else
-	echo "[ NOT ALL CHAINS IN SYNC (${count} / ${numchains}), ABORTING ]"
-	echo "Please check ALL your chains are synced before running start_iguana.sh"
-fi
-
+echo "[ ALL CHAINS SYNC'd Starting Iguana... ]"
+./start_iguana.sh
