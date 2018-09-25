82b824d07774c20448552b8e59e79cdd80e12fc4
blackjok3rtt <30971146+blackjok3rtt@users.noreply.github.com>
2018-09-25 19:40:49 +0800

    add bc for smk scritpts

--- install/installSuperNET.sh
+++ install/installSuperNET.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
 sudo apt-get update
-sudo apt-get -y install jq htop tmux git slurm
+sudo apt-get -y install jq htop tmux git slurm bc
 git clone https://github.com/StakedChain/SuperNET.git -b jl777
 
