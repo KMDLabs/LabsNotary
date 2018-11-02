bd3d66369688e36c65fe755d5dd0ea1b4e0e7aa8
blackjok3rtt <blackjok3r@gmail.com>
2018-11-03 03:09:24 +0800

    Add firewall script for chains.

--- install/buildkomodo.sh
+++ install/buildkomodo.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get -y install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl4-openssl-dev bsdmainutils automake curl
 #Install Komodo
 cd ~
-git clone https://github.com/stakedchain/komodo.git
+#git clone https://github.com/stakedchain/komodo.git
 cd komodo
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh -j$(nproc)
