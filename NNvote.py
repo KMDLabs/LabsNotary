91dfa342e655764daac8fa44a771a84d1d497bf5
Alrighttt <MarkZO@gmail.com>
2019-05-05 22:08:21 +0000

    add CFEKORC, initial voting dapp

---
+++ NNvote.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import lib
+import os
+import pprint
+import sys
+
+def test_rpc(chain):
+    try:    
+        rpc_connection = lib.def_credentials(chain)
+        dummy = rpc_connection.getbalance() # test connection
+        return(rpc_connection)
+    except Exception as e:
+        os.system('clear')
+        print(e)
+        error = 'Error: Could not connect to daemon. ' + chain + ' is not running or rpc creds not found.'
+        print(error)
+        sys.exit(0)
+
+
+def print_menu(menu_list, chain, msg):
+    if isinstance(msg, dict) or isinstance(msg, list):
+        pprint.pprint(msg)
+    else: 
+        if str(msg[:5]) == 'Error':
+            print(lib.colorize(msg, 'red'))
+        else:
+            print(lib.colorize(msg, 'green'))
+    print(lib.colorize('\n' + chain, 'magenta'))
+    sync = lib.is_chain_synced(chain)
+    if sync != 0:
+        print(lib.colorize('chain not in sync ' + str(sync), 'red'))
+    print(lib.colorize('===============', 'blue'))
+    menu_item = 0
+    for i in menu_list:
+        print(str(menu_item) + ' | ' + str(i))
+        menu_item += 1
+    print('\nq | Exit TUI')
+    print(lib.colorize('===============\n', 'blue'))
+
+
+def vote_loop(chain, msg):
+    os.system('clear')
+    rpc_connection = test_rpc(chain)
+    while True:
+        os.system('clear')
+        print_menu(vote_menu, chain, msg)
+        selection = lib.user_inputInt(0,len(vote_menu),"make a selection:")
+        if int(selection) == 0:
+            msg = lib.list_active_polls(rpc_connection)
+            vote_loop(chain, msg)
+        elif int(selection) == 1:
+            msg = vote_selection(chain, '', 'register')#stakerlib.dil_listunspent(rpc_connection, 0)
+            vote_loop(chain, msg)
+        elif int(selection) == 2:
+            msg = vote_selection(chain, '', 'vote')#stakerlib.dil_listunspent(rpc_connection, 0)
+            vote_loop(chain, msg)
+        elif int(selection) == 3:
+            msg = 'ok'#stakerlib.dl_external_balance(rpc_connection)
+            vote_loop(chain, msg)
+        elif int(selection) == 4:
+            msg = lib.create_poll(rpc_connection)#stakerlib.dil_external_balance(rpc_connection)
+            vote_loop(chain, msg)
+
+def vote_selection(chain, msg, reg_or_vote):
+    os.system('clear')
+    rpc_connection = test_rpc(chain)
+    active_polls = lib.list_active_polls(rpc_connection)
+    if str(active_polls).startswith('Error'):
+        vote_loop(chain, str(active_polls))
+    os.system('clear')
+    print_menu(active_polls, chain, 'Please select a poll to ' + reg_or_vote)
+    selection = lib.user_inputInt(0,len(active_polls)-1,"make a selection:")
+    input('hold')
+    if reg_or_vote == 'vote':
+        msg = option_selection(chain, active_polls[selection])
+    elif reg_or_vote == 'register':
+        msg = lib.vote_register(rpc_connection, active_polls[selection])
+    vote_loop(chain, msg)
+
+def option_selection(chain, poll): 
+    os.system('clear')
+    rpc_connection = test_rpc(chain)
+    options = poll['options']
+    options.append('subjective')
+    print_menu(options, chain, poll['question'] + '\nPlease select your position.')
+    selection = lib.user_inputInt(0,len(options)-1,"make a selection:")
+    msg = lib.vote(rpc_connection, options[selection], poll['txid'])
+    vote_loop(chain, msg)
+    
+vote_menu = ['List active polls', 'Register to poll', 'Vote', 'Voting results', 'Create new poll']
+
+vote_loop('CFEKORC', '')
---
+++ lib.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+import json
+import re
+import os
+import platform
+import sys
+import ast
+import codecs
+from datetime import datetime
+from slickrpc import Proxy
+import bitcoin
+from bitcoin.wallet import P2PKHBitcoinAddress
+from bitcoin.core import x
+from bitcoin.core import CoreMainParams
+
+class CoinParams(CoreMainParams):
+    MESSAGE_START = b'\x24\xe9\x27\x64'
+    DEFAULT_PORT = 7770
+    BASE58_PREFIXES = {'PUBKEY_ADDR': 60,
+                       'SCRIPT_ADDR': 85,
+                       'SECRET_KEY': 188}
+
+bitcoin.params = CoinParams
+
+
+# define data dir
+def def_data_dir():
+    operating_system = platform.system()
+    if operating_system == 'Darwin':
+        ac_dir = os.environ['HOME'] + '/Library/Application Support/Komodo'
+    elif operating_system == 'Linux':
+        ac_dir = os.environ['HOME'] + '/.komodo'
+    elif operating_system == 'Windows':
+        ac_dir = '%s/komodo/' % os.environ['APPDATA']
+    return(ac_dir)
+
+
+# fucntion to define rpc_connection
+def def_credentials(chain):
+    rpcport = '';
+    ac_dir = def_data_dir()
+    if chain == 'KMD':
+        coin_config_file = str(ac_dir + '/komodo.conf')
+    else:
+        coin_config_file = str(ac_dir + '/' + chain + '/' + chain + '.conf')
+    with open(coin_config_file, 'r') as f:
+        for line in f:
+            l = line.rstrip()
+            if re.search('rpcuser', l):
+                rpcuser = l.replace('rpcuser=', '')
+            elif re.search('rpcpassword', l):
+                rpcpassword = l.replace('rpcpassword=', '')
+            elif re.search('rpcport', l):
+                rpcport = l.replace('rpcport=', '')
+    if len(rpcport) == 0:
+        if chain == 'KMD':
+            rpcport = 7771
+        else:
+            print("rpcport not in conf file, exiting")
+            print("check " + coin_config_file)
+            exit(1)
+
+    return (Proxy("http://%s:%s@127.0.0.1:%d" % (rpcuser, rpcpassword, int(rpcport))))
+
+
+def colorize(string, color):
+    colors = {
+        'blue': '\033[94m',
+        'magenta': '\033[95m',
+        'green': '\033[92m',
+        'red': '\033[91m'
+    }
+    if color not in colors:
+        return string
+    else:
+        return colors[color] + string + '\033[0m'
+
+
+def is_chain_synced(chain):
+    rpc_connection = def_credentials(chain)
+    getinfo_result = rpc_connection.getinfo()
+    blocks = getinfo_result['blocks']
+    longestchain = getinfo_result['longestchain']
+    if blocks == longestchain:
+        return(0)
+    else:
+        return([blocks, longestchain])
+
+
+def user_inputInt(low,high, msg):
+    while True:
+        user_input = input(msg)
+        if user_input == ('q' or 'quit'):
+            print('Exiting...')
+            sys.exit(0)
+        try:
+            number = int(user_input)
+        except ValueError:
+            print("integer only, try again")
+            continue
+        if low <= number <= high:
+            return number
+        else:
+            print("input outside range, try again")
+
+
+
+def notary_list(rpc):
+    iguana_json = rpc.getiguanajson()
+    notary_list = []
+    for notary in iguana_json['notaries']:
+        templist = []
+        for i in notary:
+            templist.append(i)
+            templist.append(notary[i])
+            notary_list.append(templist)
+    return(notary_list) 
+
+
+def list_active_polls(rpc):
+    try:
+        mypk = rpc.setpubkey()['pubkey']
+    except:
+        return('Error: -pubkey not set')
+
+    notarylist = notary_list(rpc)
+    NN_pks = []
+    for notary in notarylist:
+        NN_pks.append(notary[1])
+    if not mypk in NN_pks:
+        return('Error: -pubkey is not a notary pubkey.')
+
+    oracles = rpc.oracleslist()
+    polls = []
+    for oracle in oracles:
+        vote_info = {}
+        publishers = []
+        oracleinfo = rpc.oraclesinfo(oracle)
+
+        # find pubkey that created the oracle
+        # this relies on the change output from oraclescreate
+        oraclecreate_decode = rpc.getrawtransaction(oracle, 2)
+        try:
+            creator_pk = oraclecreate_decode['vout'][1]['scriptPubKey']['hex'][2:][:-2]
+            creator_addr = P2PKHBitcoinAddress.from_pubkey(x(creator_pk))
+        except Exception as e:
+            continue
+
+        # FIXME CHANGE TO VOTE
+        if oracleinfo['name'][-5:] == 'VOTET':
+            # check that signed message matches key
+            sig = oracleinfo['description'][:88]
+            msg = oracleinfo['description'][88:]
+            try:
+                verify = rpc.verifymessage(str(creator_addr), sig, msg)
+            except:
+                continue
+            if not verify:
+                continue
+
+            # check that the key is a notary
+            if creator_pk in NN_pks:
+               vote_info['name'] = oracleinfo['name'][:-6] # FIXME CHANGE TO VOTE
+               desc_dict = ast.literal_eval(oracleinfo['description'][88:])
+               vote_info['question'] = desc_dict['question']
+               vote_info['options'] = desc_dict['options']
+               vote_info['created'] = oraclecreate_decode['blocktime']#datetime.utcfromtimestamp(oraclecreate_decode['blocktime']).strftime('%D %H:%M')
+               vote_info['deadline'] = oraclecreate_decode['blocktime'] + 604800#datetime.utcfromtimestamp(oraclecreate_decode['blocktime'] + 604800).strftime('%D %H:%M')
+               vote_info['txid'] = oracle
+               for i in notarylist:
+                   if i[1] == creator_pk:
+                       creator = i[0]
+               vote_info['creator'] = creator 
+               polls.append(vote_info)
+    #FIXME check deadline, only include active
+    return(polls)
+    
+
+
+def create_poll(rpc):
+    try:
+        mypk = rpc.setpubkey()['pubkey']
+    except:
+        return('Error: -pubkey not set')
+    mypk_addr = rpc.setpubkey()['address']
+
+    notarylist = notary_list(rpc)
+    NN_pks = []
+    for notary in notarylist:
+        NN_pks.append(notary[1])
+    if not mypk in NN_pks:
+        return('Error: -pubkey is not a notary pubkey.')
+
+    print('Please be as objective as possible. ' +
+          '>50% of notaries must vote for a change for it to be implemented. '+ 
+          'A notary can also vote \"subjective\" to signify ' +
+          'they believe poll question should be clarified and reasked. ' +
+          'Polls will have a voting deadline of 1 week for now.\n')
+
+    options = []
+    option_count = user_inputInt(1,10, 'Please input the number of options for the poll. '
+                                       'This number does not include the \"subjective\" option: ')
+    poll_name = input('Please input a name for this poll: ') + '_VOTET' # FIXME CHANGE TO VOTE
+    question = input('Please input the full question you wish to ask. Be as clear and objective as possible: ')
+    for i in range(option_count):
+        options.append(input('Please input option ' + str(i) + ': '))
+    
+    description = {}
+    description['question'] = question
+    description['options'] = options
+    print('\nname:', poll_name)
+    print(description)
+    yn = input('\nPlease confirm this is correct(y/n):')
+    if not yn.startswith('y'):
+        return('Cancelled poll creation')
+    
+    # create a signed message from notary key to prevent impersonation
+    signed = rpc.signmessage(mypk_addr, str(description))
+
+    try:
+        oraclescreate = rpc.oraclescreate(poll_name, signed + str(description), 'S')['hex']
+    except Exception as e:
+        return('Error: oraclescreate rpc command failed with ' + str(e))
+
+    #test that vout1 is change vout so creator can be determined
+    decode = rpc.decoderawtransaction(oraclescreate)
+    if decode['vout'][1]['scriptPubKey']['type'] == 'nulldata':
+        return('Error: Please consolidate UTXOs.') 
+
+    txid = rpc.sendrawtransaction(oraclescreate)
+    return('Success! Poll created at ' + txid )
+    
+def oraclesdata_encode(message):
+    rawhex = codecs.encode(message).hex()
+
+    #get length in bytes of hex in decimal
+    bytelen = int(len(rawhex) / int(2))
+    hexlen = format(bytelen, 'x')
+
+    #get length in big endian hex
+    if bytelen < 16:
+        bigend = "000" + str(hexlen)
+    elif bytelen < 256:
+        bigend = "00" + str(hexlen)
+    elif bytelen < 4096:
+        bigend = "0" + str(hexlen)
+    elif bytelen < 65536:
+        bigend = str(hexlen)
+
+    #convert big endian length to little endian, append rawhex to little endian length
+    lilend = bigend[2] + bigend[3] + bigend[0] + bigend[1]
+    fullhex = lilend + rawhex
+    return(fullhex)
+
+
+def vote_register(rpc, poll):
+    txid = poll['txid']
+    oracleinfo = rpc.oraclesinfo(txid)
+    try:
+        mypk = rpc.setpubkey()['pubkey']
+    except Exception as e:
+        return('Error: -pubkey is not set' + str(e))
+
+    # register to poll oracle
+    try:
+        oraclereg = rpc.oraclesregister(txid, '10000')
+        oraclereg_hex = oraclereg['hex']
+    except Exception as e:
+        return('Error: oraclesregister rpc command failed with ' + str(e))
+    reg_txid = rpc.sendrawtransaction(oraclereg_hex)
+
+    # subscribe to self on poll oracle
+    try:
+        oraclesub = rpc.oraclessubscribe(txid, mypk, '0.00010000')
+        sub_hex = oraclesub['hex']
+    except Exception as e:
+        return('Error: oraclessubscribe rpc command failed with ' + str(e))
+    sub_txid = rpc.sendrawtransaction(sub_hex)
+    return('Success! Please wait for ' + sub_txid + ' to be confirmed before attempting to vote.')
+
+
+# FIXME add check to see if already voted
+def vote(rpc, option, txid):
+    oracleinfo = rpc.oraclesinfo(txid)
+    try:
+        mypk = rpc.setpubkey()['pubkey']
+    except Exception as e:
+        return('Error: -pubkey is not set' + str(e))
+
+    # register to poll oracle
+    try:
+        oraclereg = rpc.oraclesregister(txid, '10000')
+        oraclereg_hex = oraclereg['hex']
+    except Exception as e:
+        return('Error: oraclesregister rpc command failed with ' + str(e))
+    reg_txid = rpc.sendrawtransaction(oraclereg_hex)
+
+    # subscribe to self on poll oracle
+    try:
+        oraclesub = rpc.oraclessubscribe(txid, mypk, '0.00010000')
+        sub_hex = oraclesub['hex']
+    except Exception as e:
+        return('Error: oraclessubscribe rpc command failed with ' + str(e))
+    sub_txid = rpc.sendrawtransaction(sub_hex)
+
+
+    oracle_hex = oraclesdata_encode(option)
+    try:
+        oraclesdata = rpc.oraclesdata(txid, oracle_hex)
+        data_hex = oraclesdata['hex']
+    except Exception as e:
+        return('Error: oraclesdata rpc command failed with ' + str(e))
+
+    yn = input('You selected \"' + option + '\" for the poll, \"' + oracleinfo['name'][:-6] + 
+          '\"\nPlease confirm this is correct(y/n): ')
+    if not yn.startswith('y'):
+        return('Vote cancelled. Try again.')
+    
+    oraclesdata_txid = rpc.sendrawtransaction(data_hex)
+
+    return('Success! You voted \"' + option + '\"\n' + 'txid: ' + oraclesdata_txid)
+
