e1d59547b8626db32635e3ced8a82d60ee852fef
blackjok3r <blackjok3r@gmail.com>
2019-08-20 21:03:22 +0800

    stop specific iguana

--- stop.sh
+++ stop.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}" || exit
-
-./listlizards.py | while read branch; do
-    echo "[$branch] Stopping Iguana... "
-    kill -15 $(pgrep -af "iguana ${branch}.json" | awk '{print $1}') > /dev/null 2>&1
-done
-./assets-cli stop
-komodo-cli stop
+if [[ ! -z $1 ]]; then
+    echo "[$1] Stopping Iguana... "
+    kill -15 $(pgrep -af "iguana ${1}.json" | awk '{print $1}') > /dev/null 2>&1
+else 
+    ./listlizards.py | while read branch; do
+        echo "[$branch] Stopping Iguana... "
+        kill -15 $(pgrep -af "iguana ${branch}.json" | awk '{print $1}') > /dev/null 2>&1
+    done
+    ./assets-cli stop
+    komodo-cli stop
+fi
