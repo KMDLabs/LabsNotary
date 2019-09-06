4b48da0952d2de41331fec77ee6aa3c2a8840820
blackjok3r <blackjok3r@gmail.com>
2019-09-07 02:31:09 +0800

    fix dwy

--- dwy_attack_mitigation.sh
+++ dwy_attack_mitigation.sh
@@ -6,13 +6,13 @@ addr=$(./printkey.py Radd)
   i=0
   echo "[$coin] Cleaning Wallet"
   while [[ $clean = 0 ]]; do
-    result=$(komodo-cli -ac_name=$coin z_mergetoaddress '["ANY_TADDR"]' $addr 0.0001 0 0 1000000 2> /dev/null)
+    result=$(komodo-cli -ac_name=$coin z_mergetoaddress '["ANY_TADDR"]' $addr 0.0001 0 0 1000000) # 2> /dev/null
     outcome=$(echo $?)
     if [[ $outcome = 6 ]]; then
       clean=1
     elif [[ $outcome = 0 ]]; then
       echo "[$coin] $(echo $result | jq -r .remainingUTXOs) utxo remaining"
-    elif [[ $outcome = 1 ]] || (( i < 10)); then
+  elif [[ $outcome = 1 ]] && (( i < 10 )); then
       echo "[$coin] Chain Syncing... waiting $i..."
       sleep 1
       i=$((i+1))
