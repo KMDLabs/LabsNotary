3fb53809218a21ba3b5e3456a4458f03a75ed206
blackjok3r <blackjok3r@gmail.com>
2019-12-11 16:36:53 +0800

    fix listassetchains

--- listassetchains.py
+++ listassetchains.py
@@ -13,6 +13,9 @@ with open(script_dir + '/assetchains.json') as file:
     assetchains = json.load(file)
 
 for chain in assetchains:
-    if specific_iguana and chain['iguana'] != specific_iguana:
+    iguana = "blackjok3r"
+    if iguana in chain:
+        iguana = chain['iguana']
+    if specific_iguana and iguana != specific_iguana:
         continue
     print(chain['ac_name'])
